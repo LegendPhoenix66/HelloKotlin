4bfd1d2d3e012577bf6f66e746a5942cad8ed5ab
LegendPhoenix66 <louis@hopp.lu>
2024-02-16 03:05:10 +0100

    making name nullable

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,7 @@
 fun main() {
-    var name: String = "John";
-    name = "Doe"; // Error: Val cannot be reassigned
+    var name: String? = "John";
+    name = "Doe";
+    name = null;
 
     println("Name: $name");
 }
