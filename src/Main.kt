bd3297fc5cad70240f306714c2ffc5c3efc77bee
LegendPhoenix66 <louis@hopp.lu>
2024-02-16 04:00:56 +0100

    only print greeting if it's not null

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,8 @@
 val name = "John Doe";
 var greeting : String? = null;
 fun main() {
-    println(greeting);
-    println(name);
-
-    greeting = null;
-    greeting = "Hello";
-
-    println(greeting);
+    if (greeting != null) {
+        println(greeting);
+    }
     println(name);
 }
