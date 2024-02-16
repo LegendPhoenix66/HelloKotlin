5f8c10e64522686ef3bb642ee640161843d1b59f
LegendPhoenix66 <louis@hopp.lu>
2024-02-16 03:20:58 +0100

    back to the first example, now with the variable outside the function

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,4 @@
-fun calculateDimensions(length: Int, width: Int, height: Int, depth: Int, radius: Int, diameter: Int) {
-    println("Length: $length")
-    println("Width: $width")
-    println("Height: $height")
-    println("Depth: $depth")
-    println("Radius: $radius")
-    println("Diameter: $diameter")
-}
-
+val name: String = "John Doe";
 fun main() {
-    calculateDimensions(10, width = 20, 30, depth = 40, 50, diameter = 60)
+    println(name);
 }
