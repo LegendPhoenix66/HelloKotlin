d62ab0c906d15ed14117149783716aba54cdb10d
LegendPhoenix66 <louis@hopp.lu>
2024-02-16 03:08:44 +0100

    adding parameter and condition

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,13 @@
 fun main() {
-    val name: String = getName() ?: "No Name";
+    val name: String = getName(20) ?: "No Name";
 
     println("Name: $name");
 }
 
-fun getName(): String? {
-    return null;
+fun getName(age: Int): String? {
+    if (age > 18) {
+        return "John Doe";
+    } else {
+        return null;
+    }
 }
